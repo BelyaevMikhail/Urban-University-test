d9493e7878b648e6b2c214f3e31e5bc3bbeb7959
Belyaev Mikhail <68550956+BelyaevMikhail@users.noreply.github.com>
2024-08-05 00:10:09 +0300

    Create module_1_3.py

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Mikhail"
+age = 52
+new_age = age + 5
+is_student = True
+print("Name: " + name)
+print("Age: " + str(age))
+print("New Age: " + str(new_age))
+print("Is Student: " + str(is_student))
